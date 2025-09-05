7672d59768d3f032508408a3783b68b5c75ed0b4
Alexander Nikulin <alexclend@gmail.com>
2025-09-05 11:26:05 +0300

    run app and service

--- test.py
+++ test.py
@@ -1,13 +1,31 @@
+import sys
 from datatailr.scheduler import Job, EntryPoint, JobType
-from service import main
+from service import main as service_main
+from app import main as app_main
 
-service = Job(
-    "test_service",
-    type=JobType.SERVICE,
-    entrypoint=EntryPoint(
+arg = sys.argv[1] if len(sys.argv) > 1 else 'service'
+
+if arg == 'service':
+    job = Job(
+        "test_service",
         type=JobType.SERVICE,
-        func=main,
-    ),
-    run_as="an",
-)
-service.save()
+        entrypoint=EntryPoint(
+            type=JobType.SERVICE,
+            func=service_main,
+        ),
+        run_as="an",
+    )
+    job.save()
+elif arg == 'app':
+    job = Job(
+        "test_app",
+        type=JobType.APP,
+        entrypoint=EntryPoint(
+            type=JobType.APP,
+            func=app_main,
+        ),
+        run_as="an",
+    )
+    job.save()
+else:
+    print("Unknown argument. Use 'app' or 'service'.")
